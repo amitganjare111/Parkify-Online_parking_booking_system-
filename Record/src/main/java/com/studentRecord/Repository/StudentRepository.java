018440c9ecdb327e43eaef5474f6f182e38762ff
Amit Ganjare <amitganjare111@gmail.com>
2023-07-19 11:00:17 +0530

    parkify_commit

--- Record/src/main/java/com/studentRecord/Repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.studentRecord.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.studentRecord.Entity.Student;
-
-public interface StudentRepository extends JpaRepository<Student, Integer> {
-
-}
